ef6ea5436e77423e1b1fb2c1545beadc2b49816a
Bin Wang <bwang28c@gmail.com>
2022-03-19 17:21:40 +0800

    update

--- src/w_evaluation.py
+++ src/w_evaluation.py
@@ -36,6 +36,7 @@ class Word_emb_evaluator:
         self.word_pairs_data = word_pairs_data
         self.word_emb_model  = word_emb_model
 
+
     def eval(self):
         ''' main functions for this module '''
         
@@ -102,86 +103,57 @@ class Word_emb_evaluator:
     def eval_for_ranking(self):
         ''' evaluate the embeddings (ori, post) on ranking task '''
 
-        vocab_embs_ori  = [self.word_emb_model.compute_embedding(word)[0] for word in self.word_pairs_data.vocab]
-        vocab_embs_ori  = np.stack(vocab_embs_ori)
-        vocab_embs_post = [self.word_emb_model.compute_embedding(word)[1] for word in self.word_pairs_data.vocab]
-        vocab_embs_post = np.stack(vocab_embs_post)
+        ranks      = []
+        vocab_embs = [self.word_emb_model.compute_embedding(word) for word in self.word_pairs_data.vocab]
+        vocab_embs = np.stack(vocab_embs)
 
-        ranks_ori  = []
-        ranks_post = []
+        for pair in tqdm(self.word_pairs_data.pos_pairs, leave=False):
+        # for pair in self.word_pairs_data.pos_pairs:
 
-        if self.dist_metric == 'mahalanobis': pre_w1 = None
+            w1, w2 = pair
+            w1_emb = self.word_emb_model.compute_embedding(w1)
+            w2_emb = self.word_emb_model.compute_embedding(w2)
 
-        #for pair in tqdm(self.word_pairs_data.pos_pairs, leave=False):
-        for pair in self.word_pairs_data.pos_pairs:
-
-            w1        , w2          = pair
-            w1_emb_ori, w1_emb_post = self.word_emb_model.compute_embedding(w1)
-            w2_emb_ori, w2_emb_post = self.word_emb_model.compute_embedding(w2)
-
-            # results for ori & post embeddings
-            if self.dist_metric == 'cos' or self.dist_metric == 'inner':
+            if self.dist_metric == 'cos':
             
-                pos_score_ori         = np.dot(w1_emb_ori, w2_emb_ori)
-                background_scores_ori = np.dot(vocab_embs_ori, w1_emb_ori)
-                background_scores_ori = np.sort(background_scores_ori)[::-1]
-
-                pos_score_post         = np.dot(w1_emb_post, w2_emb_post)
-                background_scores_post = np.dot(vocab_embs_post, w1_emb_post)
-                background_scores_post = np.sort(background_scores_post)[::-1]
+                pos_score         = np.dot(w1_emb, w2_emb)
+                background_scores = np.dot(vocab_embs, w1_emb)
+                background_scores = np.sort(background_scores)[::-1]
             
             elif self.dist_metric == 'l2':
             
-                pos_score_ori         = 1 / (np.linalg.norm(w1_emb_ori - w2_emb_ori) + 1)
-                background_scores_ori = 1 / (np.linalg.norm((vocab_embs_ori - w1_emb_ori),axis=1) + 1)
-                background_scores_ori = np.sort(background_scores_ori)[::-1]
-
-                pos_score_post         = 1 / (np.linalg.norm(w1_emb_post - w2_emb_post) + 1)
-                background_scores_post = 1 / (np.linalg.norm((vocab_embs_post - w1_emb_post),axis=1) + 1)
-                background_scores_post = np.sort(background_scores_post)[::-1]
+                pos_score_ori     = 1 / (np.linalg.norm(w1_emb - w2_emb) + 1)
+                background_scores = 1 / (np.linalg.norm((vocab_embs - w1_emb),axis=1) + 1)
+                background_scores = np.sort(background_scores)[::-1]
 
             else: 
                 sys.exit("Distance Metric NOT SUPPORTED: {}".format(self.dist_metric))
             
-            
-            rank_ori = len(background_scores_ori) - np.searchsorted(background_scores_ori[::-1], pos_score_ori, side='right')
-            if rank_ori == 0: rank_ori = 1
-            ranks_ori.append(int(rank_ori))
+            rank = len(background_scores) - np.searchsorted(background_scores[::-1], pos_score_ori, side='right')
+            if rank == 0: rank = 1
+            ranks.append(int(rank))
 
-            rank_post = len(background_scores_post) - np.searchsorted(background_scores_post[::-1], pos_score_post, side='right')
-            if rank_post == 0: rank_post = 1
-            ranks_post.append(int(rank_post))
+        MR  = np.mean(ranks)
+        MRR = np.mean(1. / np.array(ranks))
 
-
-        MR_ori   = np.mean(ranks_ori)
-        MRR_ori  = np.mean(1. / np.array(ranks_ori))
-        MR_post  = np.mean(ranks_post)
-        MRR_post = np.mean(1. / np.array(ranks_post))
-
-        hits_scores_ori  = []
-        hits_scores_post = []
-        hits_max_bound   = 15
+        hits_scores    = []
+        hits_max_bound = 15
         
         for i in range(hits_max_bound):
-            hits_scores_ori.append(sum(np.array(ranks_ori)<=(i+1))/len(ranks_ori))
-            hits_scores_post.append(sum(np.array(ranks_post)<=(i+1))/len(ranks_post))
+            hits_scores.append(sum(np.array(ranks)<=(i+1))/len(ranks))
 
         res_rank = {
-                    'MR_ori'    : MR_ori,
-                    'MRR_ori'   : MRR_ori,
-                    'MR_post'    : MR_post,
-                    'MRR_post'   : MRR_post}
+                    'MR_ori'    : MR,
+                    'MRR_ori'   : MRR,}
 
-        for i in range(hits_max_bound):
-            res_rank['hits_'+str(i+1)+'_ori']  = hits_scores_ori[i]
-            res_rank['hits_'+str(i+1)+'_post'] = hits_scores_post[i]
+        for i in range(hits_max_bound): res_rank['hits_'+str(i+1)+'_ori']  = hits_scores[i]
 
 
-        table = PrettyTable(['Scores', 'Ori Emb', 'Post Emb'])
-        table.add_row(['MR',         MR_ori,       MR_post])
-        table.add_row(['MRR',        MRR_ori,      MRR_post])
+        table = PrettyTable(['Scores', 'Emb'])
+        table.add_row(['MR', MR])
+        table.add_row(['MRR', MRR])
         
         for i in range(hits_max_bound):
-            table.add_row(['Hits@'+str(i+1), res_rank['hits_'+str(i+1)+'_ori'], res_rank['hits_'+str(i+1)+'_post']])
+            table.add_row(['Hits@'+str(i+1), res_rank['hits_'+str(i+1)+'_ori']])
 
         return table, res_rank
