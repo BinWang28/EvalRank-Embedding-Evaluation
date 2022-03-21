3d1b91469ec0399df2591b43abf7f630c8c2c20f
Bin Wang <bwang28c@gmail.com>
2022-03-21 17:20:11 +0800

    update

--- environment.sh
+++ environment.sh
@@ -2,30 +2,23 @@
 
 
 # create a new virtual environment with conda (without asking confirmation)
-
 conda create -n evalrank_env python=3.8 --yes
 
 # activate
-
 conda activate evalrank_env
 
 # word evaluation essentials
-
 conda install -c anaconda scikit-learn --yes
-
 conda install -c conda-forge tqdm --yes
-
 conda install -c conda-forge prettytable --yes
 
 
-# proper pytorch environment
+# Pytorch
+conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
 
-conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch --yes
+conda install -c conda-forge python-dateutil
 
-conda install -c conda-forge python-dateutil --yes
-
-#conda install -c huggingface transformers==4.11.3 --yes
-
+# install transformers
 
 pip install httplib2
 pip install transformers==4.11.3
